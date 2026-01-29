3ec720b524d580ea92950764771dd4defcc85d9e
WhiteCode05 <raffiblancofebbraio@gmail.com>
2026-01-30 00:18:33 +0100

    reformatt del progetto completamente

--- TestUdemy/Source/TestUdemy/TestUdemy.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestUdemy.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestUdemy, "TestUdemy" );
